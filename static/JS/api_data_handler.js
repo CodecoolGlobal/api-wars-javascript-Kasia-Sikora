b2a76275228e9eeb3f32ea18e040b40381a2ed06
ruda <sikora.katarzyna.waw@gmail.com>
2019-11-03 15:46:45 +0100

    Refactor

--- static/JS/api_data_handler.js
+++ static/JS/api_data_handler.js
@@ -1,105 +1,75 @@
+import { fetchData } from "./api_connection.js";
+
 export const data_handler = {
+
+    table: document.getElementById('main_table'),
+    prev: '',
+    next: '',
+    callbackfn: () => {},
 
     pageUp: () => {
         console.log('wchodzi');
-        // page = planets_datas.next;
-        // startScript = 0;
-        // while (table.hasChildNodes()) {
-        //     table.removeChild(table.firstChild)
-        // };
-        // load()
+        data_handler.table.innerHTML = '';
+        fetchData(data_handler.next, data_handler.callbackfn);
 },
 
     pageDown: () => {
-            console.log("wchodzi prev");
-            // page = planets_datas.previous;
-            // startScript = 0;
-            // while (table.hasChildNodes()) {
-            //     table.removeChild(table.firstChild)
-            // };
-            // load()
-}
+        console.log("wchodzi prev");
+        data_handler.table.innerHTML = '';
+        fetchData(data_handler.prev, data_handler.callbackfn);
+},
+    createColumn(data, table) {
+
+        const row = document.createElement('tr');
+        table.appendChild(row);
+
+        const table_headers = [data.name, data.diameter, data.climate, data.terrain,
+                                data.surface_water, data.population, data.residents, ""];
+
+        for (let i = 0; i < table_headers.length; i++) {
+            let col = document.createElement('td');
+            row.appendChild(col);
+
+            if (table_headers[i] === data.residents) {
+                if (data.residents.length === 0) {
+                    col.innerText = "No known residents"
+                } else {
+                    createResidentsButton(col, table_headers, i);
+                }
+            } else {
+                col.innerText = table_headers[i]
+            }
+            if (table_headers[i] === "") {
+                createVoteButton(col);
+            }
+            if (table_headers[i] === data.population) {
+                col.innerText = table_headers[i] + " people"
+            }
+            if (table_headers[i] === data.diameter) {
+                col.innerText = table_headers[i] + " km"
+            }
+            if (table_headers[i] === data.surface_water) {
+                col.innerText = table_headers[i] + "%"
+            }
+        }
+
+    },
 };
 
-import { fetchData } from "./api_connection.js";
+function createResidentsButton (col, table_headers, i){
+    const button = document.createElement('button');
+    button.dataset.type = "button";
+    button.dataset.target = '#exampleModalCenter';
+    button.dataset.toggle = 'modal';
+    button.setAttribute("class", 'button');
+    button.innerText = table_headers[i].length + ' resident(s)';
+    col.appendChild(button);
+}
 
-
-
-
-
-
-// function variable(){
-//     const page_details = fetchData()
-// }
-//
-// const planets_datas = fetchData(page, variable());
-
-
-//
-//
-//
-//
-// let page = 'https://swapi.co/api/planets';
-// console.log(page + " głowna strona");
-//
-// const delay = 3000;
-//
-//
-//
-//
-//             const data_results = planets_datas.results;
-//             const table = document.getElementById('main_table');
-//             const table_headers = ["Name", "Diameter", "Climate", "Terrain", "Surface Water Percentage", "Population", "Residents", ""];
-//             const row = document.createElement('tr');
-//             table.appendChild(row);
-//
-//
-//             for (let i = 0; i < table_headers.length; i++) {
-//                 const header = document.createElement('th');
-//                 header.innerText = table_headers[i];
-//                 row.appendChild(header)
-//             }
-//
-//
-//             for (let data of data_results) {
-//                 const row = document.createElement('tr');
-//                 table.appendChild(row);
-//
-//                 const table_headers = [data.name, data.diameter, data.climate, data.terrain, data.surface_water, data.population, data.residents, ""];
-//                 for (let i = 0; i < table_headers.length; i++) {
-//                     const col = document.createElement('td');
-//                     row.appendChild(col);
-//                     if (table_headers[i] === data.residents) {
-//                         if (data.residents.length === 0) {
-//                             col.innerText = "No known residents"
-//                         } else {
-//                             const button = document.createElement('button');
-//                             button.dataset.type = "button";
-//                             button.innerText = table_headers[i].length + ' resident(s)';
-//                             col.appendChild(button);
-//                         }
-//                     } else {
-//                         col.innerText = table_headers[i]
-//                     }
-//                     if (table_headers[i] === "") {
-//                         const button = document.createElement('button');
-//                         button.dataset.type = "button";
-//                         button.innerText = "Vote";
-//                         col.appendChild(button);
-//                     }
-//                     if (table_headers[i] === data.population) {
-//                         col.innerText = table_headers[i] + " people"
-//                     }
-//                     if (table_headers[i] === data.diameter) {
-//                         col.innerText = table_headers[i] + " km"
-//                     }
-//                     if (table_headers[i] === data.surface_water) {
-//                         col.innerText = table_headers[i] + "%"
-//                     }
-//                 }
-//             }
-//
-//             console.log(planets_datas.results)
-//         });
-//
-// }
+function createVoteButton(col){
+    const button = document.createElement('button');
+    button.setAttribute("class", 'button');
+    button.dataset.type = "button";
+    button.innerText = "Vote";
+    col.appendChild(button);
+}
--- static/JS/main.js
+++ static/JS/main.js
@@ -5,6 +5,7 @@ const page = 'https://swapi.co/api/planets';
 
 fetchData(page, main_function);
 
+
 function createNextButton() {
     const nextButton = document.getElementById('nextButton');
     nextButton.addEventListener('click', data_handler.pageUp);
@@ -31,58 +32,24 @@ function createTableWithHeaders() {
 }
 
 
-function createRows(datas) {
+function createRowsAndColumns(datas) {
     let data_array = datas.results;
     console.log(data_array);
     const table = document.getElementById('main_table');
 
     data_array.forEach((data) => {
-        const row = document.createElement('tr');
-        table.appendChild(row);
-
-        const table_headers = [data.name, data.diameter, data.climate, data.terrain, data.surface_water, data.population, data.residents, ""];
-        for (let i = 0; i < table_headers.length; i++) {
-            let col = document.createElement('td');
-            row.appendChild(col);
-
-            if (table_headers[i] === data.residents) {
-                if (data.residents.length === 0) {
-                    col.innerText = "No known residents"
-                } else {
-                    const button = document.createElement('button');
-                    button.dataset.type = "button";
-                    button.setAttribute("class", 'button');
-                    button.innerText = table_headers[i].length + ' resident(s)';
-                    col.appendChild(button);
-                }
-            } else {
-                col.innerText = table_headers[i]
-            }
-            if (table_headers[i] === "") {
-                const button = document.createElement('button');
-                button.setAttribute("class", 'button');
-                button.dataset.type = "button";
-                button.innerText = "Vote";
-                col.appendChild(button);
-            }
-            if (table_headers[i] === data.population) {
-                col.innerText = table_headers[i] + " people"
-            }
-            if (table_headers[i] === data.diameter) {
-                col.innerText = table_headers[i] + " km"
-            }
-            if (table_headers[i] === data.surface_water) {
-                col.innerText = table_headers[i] + "%"
-            }
-        }
+        data_handler.createColumn(data, table);
     });
 }
 
 
 function main_function(data) {
     console.log(data);
+    data_handler.next = data.next;
+    data_handler.prev = data.previous;
+    data_handler.callbackfn = main_function;
     createNextButton();
     createPrevButton();
     createTableWithHeaders();
-    createRows(data);
+    createRowsAndColumns(data);
 }
